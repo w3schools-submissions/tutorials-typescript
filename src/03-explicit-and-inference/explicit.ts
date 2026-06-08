3016644d87fa8338322dd0f4665247e3eaaa4de7
cH0NKIIs34L <chonkyseal.personal@gmail.com>
2026-06-08 16:08:02 +0800

    feat: add explicit types

---
+++ src/03-explicit-and-inference/explicit.ts
@@ -0,0 +1,11 @@
+// String
+const greeting: string = "Hello, TypeScript!";
+
+// Number
+let userCount: number = 42;
+
+// Boolean
+let isLoading: boolean = true;
+
+// Array
+const scores: number[] = [100, 95, 98];
