4afb3a429addb080abc812ae4e76ec592740da2a
cH0NKIIs34L <chonkyseal.personal@gmail.com>
2026-06-08 16:08:10 +0800

    feat: add implicit types

---
+++ src/03-explicit-and-inference/inference.ts
@@ -0,0 +1,13 @@
+// String
+let username = "alice";
+
+// Number
+let score = 100;
+
+// Boolean Array
+let flags = [true, false, true];
+
+// Return type as Number
+function add(a: number, b: number) {
+  return a + b;
+}
