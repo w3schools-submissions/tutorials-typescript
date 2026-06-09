6633de003b4f466ebe2e54b0e5ee004094f4b11e
cH0NKIIs34L <chonkyseal.work@gmail.com>
2026-06-09 12:46:07 +0800

    feat: add array examples

---
+++ src/05-arrays/arrays.ts
@@ -0,0 +1,12 @@
+const arr: string[] = [];
+arr.push("item");
+
+// Readonly
+const names: readonly string[] = ["Dylan", "Alice", "Bob"];
+names.push("Jack");
+
+// Type Interface
+const numbers: = [1, 2, 3];
+numbers.push(4);
+numbers.push("5");
+let head: number = numbers[0];
