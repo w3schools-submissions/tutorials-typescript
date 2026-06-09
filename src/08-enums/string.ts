cf9cc31454b2f5ef4a6eb0b3ad76bcd283df9ad9
cH0NKIIs34L <chonkyseal.personal@gmail.com>
2026-06-09 17:52:39 +0800

    feat(enums): add string examples

---
+++ src/08-enums/string.ts
@@ -0,0 +1,8 @@
+enum CardinalDirections {
+  North = "North",
+  South = "South",
+  East = "East",
+  West = "West",
+}
+console.log(CardinalDirections.North);
+console.log(CardinalDirections.West);
